b640fb55568fcddcd66259e8c7d87bd39efddf83
derberg <lpgornicki@gmail.com>
2021-12-28 11:52:01 +0100

    asyncapi v0.11.2 (new formula)

---
+++ Formula/asyncapi.rb
@@ -0,0 +1,25 @@
+require "language/node"
+
+class AsyncapiCli < Formula
+  desc "All in one CLI for all AsyncAPI tools"
+  homepage "https://github.com/asyncapi/cli"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.11.2.tgz"
+  sha256 "75e4d0e73f0ea8693ccfa684c3fd5ca26cab4f4a634c9064b90520093df882bc"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"asyncapi", "new", "--file-name=asyncapi.yml", "--example=default-example.yaml", "--no-tty"
+    assert_predicate testpath/"asyncapi.yml", :exist?, "AsyncAPI file was not created"
+  end
+end
